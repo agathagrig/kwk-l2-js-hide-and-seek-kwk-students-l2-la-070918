d23297867f6e378d6834dab5c1b1492e8027d74b
Agatha Grig <agathagrigorian@gmail.com>
2018-07-13 17:41:20 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,4 +11,9 @@ function deepestChild(){
   return document.querySelector("#grand-node div div div div")
 }
 
-increaseRankBy(n)
+function increaseRankBy(n){
+ const lis = document.querySelectorAll('ul.ranked-list')
+ for (let i=0; i < lis.length; i++) {
+   lis[i].innerHTML = (i+1).toString()
+ }
+ return document.querySelector(".ranked-list ")
